0b25ab6ffb36c415b759532ab0fbe896f37a0a1b
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-12 07:28:39 +0000

    2025-03: change algorithm and  apply both part1 part2

--- 2025-03/src/lib.rs
+++ 2025-03/src/lib.rs
@@ -1,28 +1,48 @@
+pub(crate) fn solven(file: String, target: usize) -> u64 {
+    file.lines()
+    .map(|line: &str| {
+        // The total number of deletions we are allowed to make
+        let mut del_allown = line.len() - target;
+        let mut stack = Vec::<u64>::new();
+
+        line.chars().for_each(|c| {
+            let n = c.to_digit(10).unwrap_or(0) as u64;
+            // While the stack isn't empty, 
+            // AND the current digit is larger than the top of the stack,
+            // AND we still have deletions left to use:
+            while ! stack.is_empty() &&
+                Some(n) > stack.last().copied() &&
+                del_allown > 0
+            {
+                // Pop the smaller digit out; we are choosing to delete it
+                _ = stack.pop();
+                del_allown -= 1;
+            }
+            // Push the current digit onto the stack
+            stack.push(n);
+        });
+        // If we still have deletions left after looking at all digits,
+        // truncate the remaining allowed deletions from the end of the stack
+        while del_allown > 0 {
+            _ = stack.pop();
+            del_allown -= 1;
+        }
+        // Convert the first n elements of the stack into an integer
+        stack.truncate(target); 
+        stack.iter().fold(0, |acc, &n| acc*10 + n )
+    })
+    // .inspect(|f| {println!("{f}")})
+    .sum::<u64>() as u64
+}
+
 pub mod part1 {
     pub fn solve(file: String) -> u64 {
-        file.lines().map(|line| line.to_string())
-        .map(|line| {
-            let mut max: u64 = 0;
-            (0..line.len()).for_each(|i| {
-                (i+1..line.len()).for_each(|j| {
-                    let num = line.chars().nth(i).unwrap().to_digit(10).unwrap()*10 +
-                              line.chars().nth(j).unwrap().to_digit(10).unwrap();
-                    max = max.max(num as u64);
-                });
-            });
-            max
-        })
-        .inspect(|&n| { println!("{n}");})
-        .sum()
+        crate::solven(file, 2)
     }
 }
 
 pub mod part2 {
     pub fn solve(file: String) -> u64 {
-        file.lines().map(|line| line.to_string())
-        .map(|line| {
-            0
-        })
-        .sum()
+        crate::solven(file, 12)
     }
 }
