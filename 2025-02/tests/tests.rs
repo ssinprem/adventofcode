81aef23091effe5d035ebea3322ae1e6ff284dd8
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-15 08:52:13 +0000

    2025-02 : apply new test_cases macro
    ```
    ssinprem@AZUHPS-MetaBMC:~/Rust-Dev/adventoofcode/2025-02$ cargo test
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.04s
         Running unittests src/lib.rs (/work/home/ssinprem/Rust-Dev/adventoofcode/target/debug/deps/gift_shop-be2a5c0fe3466f20)
    
    running 0 tests
    
    test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    
         Running unittests src/main.rs (/work/home/ssinprem/Rust-Dev/adventoofcode/target/debug/deps/gift_shop-3483e58f68e1af5d)
    
    running 0 tests
    
    test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    
         Running tests/tests.rs (/work/home/ssinprem/Rust-Dev/adventoofcode/target/debug/deps/tests-d25106e07458449f)
    
    running 2 tests
    test example_part1 ... ok
    test example_part2 ... ok
    
    test result: ok. 2 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    
       Doc-tests gift_shop
    
    running 0 tests
    
    test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    ```

--- 2025-02/tests/tests.rs
+++ 2025-02/tests/tests.rs
@@ -1,7 +1,9 @@
-use gift_shop::part2::*;
-use utility::test_case;
+use gift_shop::*;
+use utility::test_cases;
 
-test_case!(example, solve, 4174379265,
+test_cases!(example, 
+    part1::solve, 1227775554,
+    part2::solve, 4174379265,
 "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
 824824821-824824827,2121212118-2121212124
