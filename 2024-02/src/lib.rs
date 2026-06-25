40c14f8cadd18857c43fab5306d484d442d18bec
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-25 07:03:41 +0000

    2024-02: cleared part2

--- 2024-02/src/lib.rs
+++ 2024-02/src/lib.rs
@@ -35,7 +35,49 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    pub fn solve(file: String) -> u64 {
+        file.lines().filter(|&line| !line.is_empty())
+        .enumerate().filter(|&(idx, line)| {
+            let list = line.split_whitespace()
+                .map(|str| str.parse::<i32>().expect("cannot parse number"))
+                .collect::<Vec<i32>>();
+
+            (-1..list.len() as isize).map(|rem| {
+                let mut new_list = list.clone();
+                if rem < 0  {
+                    new_list
+                } else {
+                    new_list.remove(rem as usize);
+                    new_list.clone()
+                }
+            }).any(|new_list| {
+                println!("{:4}. {new_list:?}", idx+1);
+                let mut pairs = new_list.windows(2);
+    
+                // find direction
+                let pair = pairs.next().expect("cannot take first pair");
+                let range: std::ops::RangeInclusive<i32> = 
+                    match pair[1] - pair[0] {
+                        1..=3  => 1..=3_i32,
+                        -3..=-1 => -3..=-1,
+                        _ => {
+                            println!("{:4}. found error {:3?} {:2} -> {line}", idx+1, pair, pair[1] - pair[0] );
+                            return false;
+                        }
+                    };
+                
+                pairs.into_iter().all(|pair| {
+                    match pair[1] - pair[0] {
+                        diff if ! range.contains(&diff) => {
+                            println!("{:4}. found error {:3?} {:2} -> {line}", idx+1, pair, diff);
+                            return false;
+                        }
+                        _ => {}
+                    }
+                    true
+                })
+            })
+        })
+        .count() as u64
     }
 }
