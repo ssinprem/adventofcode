d71880415370c16071526e4bb5770efaed9773cb
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-25 04:06:30 +0000

    2024-01 : cleared part2 👌

--- 2024-01/src/lib.rs
+++ 2024-01/src/lib.rs
@@ -1,6 +1,5 @@
-pub mod part1 {
-    pub fn solve(file: String) -> u64 {
-        let mut pairs = file.lines().filter(|line| !line.is_empty())
+fn get_list(file: String) -> (Vec<i32>, Vec<i32>) {
+    file.lines().filter(|line| !line.is_empty())
             .fold((Vec::<i32>::new(), Vec::<i32>::new()), | (mut list1, mut list2) , line | {
                 let mut spliter = line.split_whitespace(); 
                 let left = spliter.next().expect("cannot split left");
@@ -8,8 +7,13 @@ pub mod part1 {
                 list1.push(left.parse().expect("cannot parse left"));
                 list2.push(right.parse().expect("cannot parse right"));
                 (list1,list2)
-            }
-        );
+        }
+    )
+}
+
+pub mod part1 {
+    pub fn solve(file: String) -> u64 {
+        let mut pairs = crate::get_list(file);
         pairs.0.sort();
         pairs.1.sort();
         pairs.0.iter().zip(pairs.1).map(|(left, right)| {
@@ -19,7 +23,10 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    pub fn solve(file: String) -> u64 {
+        let pairs = crate::get_list(file);
+        pairs.0.iter().map(|&left| {
+            left * pairs.1.iter().filter(|&right| left == *right).count() as i32
+        }).sum::<i32>() as u64
     }
 }
--- 2024-01/tests/tests.rs
+++ 2024-01/tests/tests.rs
@@ -10,6 +10,6 @@ test_case_n!(example,
 3   9
 3   3
 ",
-    part1: (part1::solve, 2 + 1 + 0 + 1 + 2 +5),
-    part2: (part2::solve, 0)
+    part1: (part1::solve, 2 + 1 + 0 + 1 + 2 + 5),
+    part2: (part2::solve, 9 + 4 + 0 + 0 + 9 + 9)
 );
