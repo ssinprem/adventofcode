678e5a7a85aa86b7cc023e7188021bffc0eb32d3
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-26 03:11:59 +0000

    2024-03: cleared part 2

--- 2024-03/src/lib.rs
+++ 2024-03/src/lib.rs
@@ -6,7 +6,6 @@ pub mod part1 {
             r"mul\((\d+),(\d+)\)"
         ).unwrap();
 
-
         reg_group.captures_iter(file.as_str())
             .map(|c| c.extract())
             .collect::<Vec<_>>()
@@ -18,7 +17,28 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use regex::Regex;
+    pub fn solve(file: String) -> u64 {
+        let reg_group = Regex::new(
+            r"(?:mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))"
+        ).unwrap();
+
+        let mut does = true;
+        let mut sum = 0;
+        reg_group.captures_iter(file.as_str())
+            .for_each(|capture| {
+                if let Some(a) = capture.get(1) &&
+                   let Some(b) = capture.get(2) && 
+                   does 
+                {
+                       sum += a.as_str().parse::<u64>().expect("cannot parse") * 
+                              b.as_str().parse::<u64>().expect("cannot parse");
+                } else if capture.get(3).is_some() { // do()
+                    does = true;
+                } else if capture.get(4).is_some() { // don't()
+                    does = false;
+                }
+            });
+        sum
     }
 }
--- 2024-03/tests/tests.rs
+++ 2024-03/tests/tests.rs
@@ -3,6 +3,10 @@ use utility::test_case_n;
 
 test_case_n!(example,
 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
-    part1: (part1::solve, 2*4 + 5*5 + 11*8 + 8*5),
-    part2: (part2::solve, 0)
+    part1: (part1::solve, 2*4 + 5*5 + 11*8 + 8*5)
+);
+
+test_case_n!(example,
+"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    part2: (part2::solve, 2*4 + 8*5)
 );
