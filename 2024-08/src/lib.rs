1e5a33e98524b5ef14ada54cd7d543324c386131
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-30 14:25:17 +0700

    2024-08: cleared part2

--- 2024-08/src/lib.rs
+++ 2024-08/src/lib.rs
@@ -61,7 +61,54 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use super::*;
+    pub fn solve(file: String) -> u64 {
+        let (mut grid,set) = parse(file);
+        let mut antinode: HashSet<(isize, isize)> = HashSet::new();
+        let mut count =0;
+        for target in set {
+            let target_list: Vec<_> =grid.iter().enumerate()
+                .filter(|&(_i,&char)| char == target)
+                .map(|a| {
+                (
+                    (a.0 / grid.cols()) as isize,
+                    (a.0 % grid.cols()) as isize
+                )
+            }).collect();
+            println!("{target} -> {target_list:?}");
+
+            for i in 0..target_list.len() {
+                for j in i+1..target_list.len() {
+                    let item = target_list[i];
+                    let jtem = target_list[j];
+                    let divy = item.0 - jtem.0;
+                    let divx = item.1 - jtem.1;
+                    let resonances = vec![
+                        (item, ( divy,  divx)),
+                        (jtem, (-divy, -divx))
+                    ];
+                    for (ant,div) in resonances {
+                        let mut mul = 0;
+                        loop {
+                            let pos =  (ant.0 + mul * div.0, ant.1 + mul * div.1);
+                            if let Some(_char) = grid.get_mut(pos.0, pos.1) {
+                                if antinode.insert((pos.0, pos.1)) {
+                                    count += 1;
+                                    println!("✅ {count:3}  {pos:3?}");
+                                } 
+                                else {
+                                    println!("❕      {pos:3?} ready in antinode list");
+                                }
+                            } else {
+                                println!("❕      {pos:3?} is outside map");
+                                break;
+                            }
+                            mul += 1;
+                        }
+                    }
+                }
+            }
+        }
+        count // antinode.len() as u64
     }
 }
