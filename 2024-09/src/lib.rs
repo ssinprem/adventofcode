6c124ed5d6d80cd9cc2a9d1fa2b5ec82b49d450d
Sittisak Sinprem <ssinprem@celestica.com>
2026-06-30 17:55:40 +0700

    2024-09: cleared part2

--- 2024-09/src/lib.rs
+++ 2024-09/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Space {
     Free,
     Used(usize),
@@ -37,6 +37,16 @@ pub fn checksum(disk: Vec<Space>) -> u64 {
         .sum::<u64>()
 }
 
+pub fn print_disk(disk: Vec<Space>) {
+    for space in disk {
+        if space == Space::Free {
+            print!(".");
+        } else if let Space::Used(c) = space {
+            print!("{}", c);
+        }
+    }
+    println!();
+}
 pub mod part1 {
     use super::*;
     pub fn solve(file: String) -> u64 {
@@ -63,9 +73,40 @@ pub mod part1 {
 pub mod part2 {
     use super::*;
     pub fn solve(file: String) -> u64 {
-        let disk = parse(file);
+        let mut disk = parse(file.clone());
+        let max_chunk = file.len();
+        let mut skip = 0;
+        loop {
+            // find free space chuck
+            let binding = disk
+                .iter()
+                .enumerate()
+                .map(|(i, s)| (i, *s))
+                .collect::<Vec<(usize, Space)>>();
+            let chunks: Vec<_> = binding.chunk_by(|a, b| a.1 == b.1).collect();
 
-
+            if 
+                let Some(&useds) = chunks.iter().rev().skip(skip).find(
+                    |&&vec| vec.iter().all(|s| s.1!=Space::Free)) 
+               &&
+                let Some(&frees) = chunks.iter().find(|&&vec| 
+                    vec.iter().all(|s| s.1 == Space::Free
+                    && useds.len() <= vec.len()
+                ))
+               &&
+               frees[0].0 < useds[0].0
+            {
+                let free_pos = frees.iter().map(|s| s.0);
+                let used_pos = useds.iter().map(|s| s.0);
+                for (i, j) in free_pos.zip(used_pos) {
+                    disk.swap(i, j);
+                }
+                continue;
+            } else if skip > max_chunk / 2 {
+                break;
+            }
+            skip+=1;
+        }
         checksum(disk)
     }
 }
