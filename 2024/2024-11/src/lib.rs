de7c9af4741cc3132f48dc65e1ac89c65114036a
Sittisak Sinprem <ssinprem@celestica.com>
2026-07-02 11:45:19 +0700

    2024-11: cleared part2,  revise Vec to HashMap<str, count> to no need duplicate for same string,

--- 2024/2024-11/src/lib.rs
+++ 2024/2024-11/src/lib.rs
@@ -1,28 +1,48 @@
-pub fn parse(file: String) -> Vec<String> {
+use std::collections::HashMap;
+
+pub fn parse(file: String) -> HashMap<String,u64> {
+    let mut set = HashMap::<String,u64>::new();
     file.split_whitespace()
-        .map(|str| str.to_string())
-        .collect::<Vec<String>>()
+        .for_each(|str| {
+            set.entry(str.to_string())
+                .and_modify(|c| *c+=1)
+                .or_insert(1);
+        });
+    set
 }
 
-pub fn process(set: Vec<String>) -> Vec<String> {
-    set
-        .iter()
-        .flat_map(|str| match (str, str.len()) {
-            (str, _) if str == "0" => vec!["1".to_string()],
+pub fn process(set: HashMap<String, u64>) -> HashMap<String, u64> {
+    let mut new_set = HashMap::new();
+    for (str, count) in set {
+        match (str.clone(), str.clone().len()) {
+            (str, _) if str == "0" => {
+                new_set.entry("1".to_string())
+                    .and_modify(|c| *c+=count)
+                    .or_insert(count);
+            },
             (str, len) if len % 2 == 0 => {
                 let res = str.split_at(len / 2);
                 [res.0, res.1]
                     .iter()
-                    .map(|num| num.parse::<u64>().expect("cannot parse").to_string())
-                    .collect()
+                    .for_each(|s| {
+                        let new_str = 
+                            s.to_string().parse::<u64>().expect("cannot parse back")
+                            .to_string();
+                        new_set.entry(new_str)
+                            .and_modify(|c| *c+=count)
+                            .or_insert(count);
+                    });
             }
             (str, _) => {
                 let mut num = str.parse::<u64>().expect("parse digit");
                 num *= 2024;
-                vec![num.to_string()]
+                new_set.entry(num.to_string())
+                    .and_modify(|c| *c+=count)
+                    .or_insert(count);
             }
-        })
-        .collect()
+        }
+    }
+    new_set
 }
 
 pub mod part1 {
@@ -34,7 +54,7 @@ pub mod part1 {
             set = process(set.clone());
         });
 
-        set.len() as u64
+        set.values().sum::<u64>() as u64
     }
 }
 
@@ -47,6 +67,6 @@ pub mod part2 {
             set = process(set.clone());
         });
 
-        set.len() as u64
+        set.values().sum::<u64>() as u64
     }
 }
--- 2024/2024-11/tests/tests.rs
+++ 2024/2024-11/tests/tests.rs
@@ -4,5 +4,5 @@ use utility::test_case_n;
 test_case_n!(example,
 "125 17",
     part1: (part1::solve, 55312),
-    part2: (part2::solve, 0)
+    part2: (part2::solve, 65601038650482)
 );
