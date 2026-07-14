99a9c1951b5e8a6e367e8e80fca88b5c50a565b3
Sittisak Sinprem <ssinprem@celestica.com>
2026-07-14 18:09:36 +0700

    2024-22: part2 fix,  but still slow

--- 2024/2024-22/src/lib.rs
+++ 2024/2024-22/src/lib.rs
@@ -40,7 +40,82 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use crate::process;
+
+    pub fn get_sell_price(secret: u64) -> Vec<u64> {
+        let mut prices = Vec::with_capacity(2001);
+        let mut current_secret = secret;
+
+        prices.push(current_secret % 10);
+
+        for _ in 0..2000 {
+            current_secret = process(current_secret);
+            prices.push(current_secret % 10);
+        }
+        prices
+    }
+
+    pub fn get_changes(prices: &Vec<u64>) -> Vec<i64> {
+        prices.windows(2)
+            .map(|pair| {
+                pair[1] as i64 - pair[0] as i64
+            }).collect()
+    }
+
+    pub fn get_amount(prices: &Vec<u64>, changes: &Vec<i64>, seq: &[i64]) -> u64 {
+         if let Some(index) = changes.windows(4).position(|window| {
+            window == seq
+        }) {
+            // The sequence of 4 changes starts at `index` in the `changes` array.
+            // changes[index] = prices[index+1] - prices[index]
+            // ...
+            // changes[index+3] = prices[index+4] - prices[index+3]
+            // The price at the moment the sequence is complete is prices[index+4].
+            prices[index + 4]
+        } else {
+            0
+        }
+    }
+
+    pub fn solve(file: String) -> u64 {
+        println!("Parse secrets from file");
+        let secrets = file.lines()
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                line.parse::<u64>().expect("cannot parse digit")
+            }).collect::<Vec<u64>>();
+
+        let mut max = 0;
+        println!("Calculate Sell Price");
+        let price_set: Vec<Vec<u64>> = secrets.iter().map(|&secret | {
+            get_sell_price(secret)
+        }).collect();
+
+        println!("Calculate Change");
+        let changes_set: Vec<Vec<i64>> = price_set.iter().map(|prices| {
+            get_changes(prices)
+        }).collect();
+
+        println!("Find the Best Sequence");
+        for s1 in -9..=9 { 
+            for s2 in -9..=9 {
+                for s3 in -9..=9 {
+                    for s4 in -9..=9 {
+                        let amount = (0..price_set.len()).map(|id| {
+                            get_amount(
+                                &price_set[id], 
+                                &changes_set[id], 
+                                &[s1,s2,s3,s4]
+                            )
+                        }).sum();
+                        if amount > max {
+                            max = amount;
+                            println!(" {amount}  {:?} ",[s1,s2,s3,s4]);
+                        }
+                    }
+                }
+            }
+        }
+        max
     }
 }
--- 2024/2024-22/tests/tests.rs
+++ 2024/2024-22/tests/tests.rs
@@ -1,4 +1,4 @@
-use monkey_market::*;
+use monkey_market::{part2::{get_changes, get_sell_price}, *};
 use utility::test_case_n;
 
 test_case_n!(example,"
@@ -6,12 +6,11 @@ test_case_n!(example,"
 10
 100
 2024",
-    part1: (part1::solve, 37327623),
-    part2: (part2::solve, 0)
+    part1: (part1::solve, 37327623)
 );
 
 #[test]
-fn test_example() {
+fn test_function() {
     assert_eq!(prune(100000000), 16113920);
     let temp = process(123);
     assert_eq!(temp, 15887950);
@@ -34,8 +33,35 @@ fn test_example() {
     let temp = process(temp);
     assert_eq!(temp, 5908254);
 
-    assert_eq!((0..2000).fold(1, |init, _| process(init)), 8685429);
-    assert_eq!((0..2000).fold(10, |init, _| process(init)), 4700978);
-    assert_eq!((0..2000).fold(100, |init, _| process(init)), 15273692);
-    assert_eq!((0..2000).fold(2024, |init, _| process(init)), 8667524);
+    assert_eq!((0..2000).fold(1,   |init,_| process(init) ), 8685429);
+    assert_eq!((0..2000).fold(10,  |init,_| process(init) ), 4700978);
+    assert_eq!((0..2000).fold(100, |init,_| process(init) ), 15273692);
+    assert_eq!((0..2000).fold(2024,|init,_| process(init) ), 8667524);
 }
+
+#[test]
+fn short_part2() {
+    let price = get_sell_price(123);
+    let change = get_changes(&price);
+    assert_eq!(part2::get_amount(&price,&change, &[-1,-1,0,2]), 6);
+    let price = get_sell_price(1);
+    let change = get_changes(&price);
+    assert_eq!(part2::get_amount(&price,&change, &[-2,1,-1,3]), 7);
+    let price = get_sell_price(2);
+    let change = get_changes(&price);
+    assert_eq!(part2::get_amount(&price,&change, &[-2,1,-1,3]), 7);
+    let price = get_sell_price(3);
+    let change = get_changes(&price);
+    assert_eq!(part2::get_amount(&price,&change, &[-2,1,-1,3]), 0);
+    let price = get_sell_price(2024);
+    let change = get_changes(&price);
+    assert_eq!(part2::get_amount(&price,&change, &[-2,1,-1,3]), 9);
+}
+
+test_case_n!(example,"
+1
+2
+3
+2024",
+    part2: (part2::solve, 23)
+);
