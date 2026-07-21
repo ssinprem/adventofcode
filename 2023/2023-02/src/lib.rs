7669a0183f907534b387650fad6e4a6028d18ed4
Sittisak Sinprem <ssinprem@celestica.com>
2026-07-21 09:05:14 +0000

    2023-02: cleared part1

---
+++ 2023/2023-02/src/lib.rs
@@ -0,0 +1,70 @@
+use regex::Regex;
+use std::collections::HashMap;
+
+pub fn parse(file: String) -> HashMap<u32, Vec<[u32; 3]>> {
+    file.lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let mut game = Vec::new();
+            let (game_str, detail) = line.split_once(":").unwrap();
+            let id_regex = Regex::new(r"Game (\d+)").unwrap();
+            let round_regex = Regex::new(r"(\d+) (red|green|blue)").unwrap();
+            let game_id = id_regex
+                .captures(game_str)
+                .unwrap()
+                .get(1)
+                .unwrap()
+                .as_str()
+                .parse::<u32>()
+                .unwrap();
+            for round in detail.split(";") {
+                let mut nums = [0, 0, 0];
+                for cap in round_regex.captures_iter(round) {
+                    if let Some(n) = cap.get(1)
+                        && let Ok(num) = n.as_str().to_string().parse::<u32>()
+                        && let Some(color) = cap.get(2)
+                    {
+                        match color.as_str() {
+                            "red" => nums[0] = num,
+                            "green" => nums[1] = num,
+                            "blue" => nums[2] = num,
+                            _ => {}
+                        }
+                    }
+                }
+                game.push(nums);
+            }
+            (game_id, game)
+        })
+        .collect::<HashMap<_, _>>()
+}
+
+pub mod part1 {
+    use super::*;
+
+    pub fn solve(file: String) -> u64 {
+        // limit red 12, green 13, blue 14
+        let limit = [12, 13, 14];
+        let games = parse(file);
+        println!("{games:?}");
+
+        let mut sum = 0;
+        for (id, game) in games.iter() {
+            // all round should be in limit
+            if game
+                .iter()
+                .all(|round| (0..3).all(|n| round[n] <= limit[n]))
+            {
+                // adding by game id, if game is valid
+                sum += *id;
+            }
+        }
+        sum.into()
+    }
+}
+
+pub mod part2 {
+    pub fn solve(_file: String) -> u64 {
+        0
+    }
+}
---
+++ 2023/2023-02/tests/tests.rs
@@ -0,0 +1,12 @@
+use cube_conundrum::*;
+use utility::test_case_n;
+
+test_case_n!(example,
+"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    part1: (part1::solve, 8),
+    part2: (part2::solve, 0)
+);
