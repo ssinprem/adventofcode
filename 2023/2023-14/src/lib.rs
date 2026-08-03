e7026e08582f4d9100b9c2b541e7b4f560d0f8b8
Sittisak Sinprem <ssinprem@celestica.com>
2026-08-03 13:48:10 +0700

    2023-14: print the out each loop to find pattern manually

--- 2023/2023-14/src/lib.rs
+++ 2023/2023-14/src/lib.rs
@@ -1,5 +1,3 @@
-use std::{print, println};
-
 use grid::*;
 
 #[derive(Debug, PartialEq, Default, Copy, Clone)]
@@ -28,22 +26,22 @@ pub fn parse(file: String) -> Grid<Cell> {
     map
 }
 
-pub fn _display(map: &Grid<Cell>) {
+pub fn _display(map: &Grid<Cell>) -> String {
     let cols = map.cols();
     let rows = map.rows();
-
+    let mut string = String::new();
     for row in 0..rows {
         for col in 0..cols {
             let item = map.get(row, col);
             match item {
-                Some(&Cell::Cube) => print!("#"),
-                Some(&Cell::Round) => print!("O"),
-                _ => print!("."),
+                Some(&Cell::Cube) => string += "#",
+                Some(&Cell::Round) => string += "O",
+                _ => string += ".",
             }
         }
-        println!();
+        string += "\n"
     }
-    println!();
+    string
 }
 
 pub fn slide_up(map: &mut Grid<Cell>) {
@@ -56,7 +54,6 @@ pub fn slide_up(map: &mut Grid<Cell>) {
             if item == Some(&Cell::Round) {
                 let mut found = false;
                 let mut j = row - 1;
-
                 loop {
                     let jtem = map.get(j, col);
                     if jtem == Some(&Cell::Empty) {
@@ -78,24 +75,144 @@ pub fn slide_up(map: &mut Grid<Cell>) {
     }
 }
 
+pub fn slide_down(map: &mut Grid<Cell>) {
+    let rows = map.rows();
+    let cols = map.cols();
+
+    for col in 0..cols {
+        for row in 0..=rows {
+            let row = rows-row;
+            let item = map.get(row, col);
+            if item == Some(&Cell::Round) {
+                let mut found = false;
+                let mut j = row + 1;
+                loop {
+                    let jtem = map.get(j, col);
+                    if jtem == Some(&Cell::Empty) {
+                        found = true;
+                        if j == rows-1 {
+                            break;
+                        }
+                        j += 1;
+                    } else {
+                        j -= 1;
+                        break;
+                    }
+                }
+                if found {
+                    map.swap((row, col), (j, col));
+                }
+            }
+        }
+    }
+}
+
+pub fn slide_left(map: &mut Grid<Cell>) {
+    let rows = map.rows();
+    let cols = map.cols();
+
+    for row in 0..rows {
+        for col in 1..cols {
+            let item = map.get(row, col);
+            if item == Some(&Cell::Round) {
+                let mut found = false;
+                let mut j = col - 1;
+                loop {
+                    let jtem = map.get(row, j);
+                    if jtem == Some(&Cell::Empty) {
+                        found = true;
+                        if j == 0 {
+                            break;
+                        }
+                        j -= 1;
+                    } else {
+                        j += 1;
+                        break;
+                    }
+                }
+                if found {
+                    map.swap((row, col), (row, j));
+                }
+            }
+        }
+    }
+}
+
+pub fn slide_right(map: &mut Grid<Cell>) {
+    let rows = map.rows();
+    let cols = map.cols();
+
+    for row in 0..rows {
+        for col in 0..=cols {
+            let col = cols-col;
+            let item = map.get(row, col);
+            if item == Some(&Cell::Round) {
+                let mut found = false;
+                let mut j = col + 1;
+                loop {
+                    let jtem = map.get(row, j);
+                    if jtem == Some(&Cell::Empty) {
+                        found = true;
+                        if j == cols-1 {
+                            break;
+                        }
+                        j += 1;
+                    } else {
+                        j -= 1;
+                        break;
+                    }
+                }
+                if found {
+                    map.swap((row, col), (row, j));
+                }
+            }
+        }
+    }
+}
+
+pub fn score(map: &Grid<Cell>) -> u64 {
+    let rows = map.rows();
+    map.iter_rows()
+        .enumerate()
+        .map(|(row, items)| items.filter(|cell| cell == &&Cell::Round).count() * (rows - row))
+        .sum::<usize>() as u64
+}
 pub mod part1 {
     use super::*;
-
+    
     pub fn solve(file: String) -> u64 {
         let mut map = parse(file);
-        _display(&map);
+        println!("{}", _display(&map));
         slide_up(&mut map);
-        _display(&map);
+        println!("{}", _display(&map));
         let rows = map.rows();
         map.iter_rows()
             .enumerate()
             .map(|(row, items)| items.filter(|cell| cell == &&Cell::Round).count() * (rows - row))
             .sum::<usize>() as u64
+        }
+    }
+    
+pub mod part2 {
+    use super::*;
+    pub fn solve(file: String) -> u64 {
+        let mut map = parse(file);
+        let mut scores = Vec::new();
+        for i in 0..1000 {
+            slide_up(&mut map);
+            slide_left(&mut map);
+            slide_down(&mut map);
+            slide_right(&mut map);
+            println!("{}", i);
+            scores.push(score(&map));
+        }
+        println!("{:?}", scores);
+
+        let mut i = 0;
+        
+
+
+        score(&map)
     }
 }
 
-pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
-    }
-}
--- 2023/2023-14/tests/tests.rs
+++ 2023/2023-14/tests/tests.rs
@@ -1,3 +1,5 @@
+use std::println;
+
 use parabolic_reflector_dish::*;
 use utility::test_case_n;
 
@@ -13,5 +15,38 @@ O.#..O.#.#
 #....###..
 #OO..#....",
     part1: (part1::solve, 136),
-    part2: (part2::solve, 0)
+    part2: (part2::solve, 64)
 );
+
+#[test]
+fn test_slide() {
+    let string = "
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+    let mut map = parse(string.to_string());
+    println!("{}", _display(&map));
+    slide_up(&mut map);
+    slide_left(&mut map);
+    slide_down(&mut map);
+    slide_right(&mut map);
+    println!("{}", _display(&map));
+    slide_up(&mut map);
+    slide_left(&mut map);
+    slide_down(&mut map);
+    slide_right(&mut map);
+    println!("{}", _display(&map));
+    slide_up(&mut map);
+    slide_left(&mut map);
+    slide_down(&mut map);
+    slide_right(&mut map);
+    println!("{}", _display(&map));
+}
+
