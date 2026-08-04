4af6938102f932a293ff508b79ddeecfd52fbd6f
Sittisak Sinprem <ssinprem@celestica.com>
2026-08-04 14:02:12 +0700

    2023-16: cleared part2

--- 2023/2023-16/src/lib.rs
+++ 2023/2023-16/src/lib.rs
@@ -1,8 +1,6 @@
-use std::println;
-
 use grid::*;
 
-#[derive(Default, Debug, PartialEq)]
+#[derive(Default, Debug, PartialEq, Clone, Copy)]
 pub enum Cell {
     #[default]
     Empty,
@@ -16,7 +14,7 @@ pub enum Dir {
     Up = 1,
     Left = 2,
     Down = 4,
-    Right = 8
+    Right = 8,
 }
 
 pub fn parse(file: String) -> Grid<(Cell, u8, u8)> {
@@ -51,84 +49,73 @@ pub fn _display(map: &Grid<(Cell, u8, u8)>) -> String {
             for col in 0..cols {
                 for j in 0..3 {
                     if i == 0 && j == 1 {
-                        if let Some((_, from, to)) = map.get(row,col)
-                        {
-                            string +=
-                            match (from & Dir::Up as u8 != 0, to & Dir::Up as u8 != 0) {
-                                (true,true) => "⇅", 
-                                (true,false) => "↓", 
-                                (false,true) => "↑", 
-                                _ => " "
+                        if let Some((_, from, to)) = map.get(row, col) {
+                            string += match (from & Dir::Up as u8 != 0, to & Dir::Up as u8 != 0) {
+                                (true, true) => "⇅",
+                                (true, false) => "↓",
+                                (false, true) => "↑",
+                                _ => " ",
                             };
                         } else {
                             string += " ";
                         }
-                    }
-                    else if i == 2 && j == 1 {
-                        if let Some((_, from, to)) = map.get(row,col)
-                        {
-                            string +=
-                            match (from & Dir::Down as u8 != 0, to & Dir::Down as u8 != 0) {
-                                (true,true) => "⇅", 
-                                (true,false) => "↑", 
-                                (false,true) => "↓", 
-                                _ => " "
+                    } else if i == 2 && j == 1 {
+                        if let Some((_, from, to)) = map.get(row, col) {
+                            string += match (from & Dir::Down as u8 != 0, to & Dir::Down as u8 != 0)
+                            {
+                                (true, true) => "⇅",
+                                (true, false) => "↑",
+                                (false, true) => "↓",
+                                _ => " ",
                             };
                         } else {
                             string += " ";
                         }
-                    }
-                    else if i == 1 && j == 0 {
-                        if let Some((_, from, to)) = map.get(row,col)
-                        {
-                            string +=
-                            match (from & Dir::Left as u8 != 0, to & Dir::Left as u8 != 0) {
-                                (true,true) => "⇄", 
-                                (true,false) => "→", 
-                                (false,true) => "←", 
-                                _ => " "
+                    } else if i == 1 && j == 0 {
+                        if let Some((_, from, to)) = map.get(row, col) {
+                            string += match (from & Dir::Left as u8 != 0, to & Dir::Left as u8 != 0)
+                            {
+                                (true, true) => "⇄",
+                                (true, false) => "→",
+                                (false, true) => "←",
+                                _ => " ",
                             };
                         } else {
                             string += " ";
                         }
-                    }
-                    else if i == 1 && j == 2 {
-                        if let Some((_, from, to)) = map.get(row,col)
-                        {
+                    } else if i == 1 && j == 2 {
+                        if let Some((_, from, to)) = map.get(row, col) {
                             string +=
-                            match (from & Dir::Right as u8 != 0, to & Dir::Right as u8 != 0) {
-                                (true,true) => "⇄", 
-                                (true,false) => "←", 
-                                (false,true) => "→", 
-                                _ => " "
-                            };
+                                match (from & Dir::Right as u8 != 0, to & Dir::Right as u8 != 0) {
+                                    (true, true) => "⇄",
+                                    (true, false) => "←",
+                                    (false, true) => "→",
+                                    _ => " ",
+                                };
                         } else {
                             string += " ";
                         }
-                    }
-                    else if i == 1 && j == 1 {
+                    } else if i == 1 && j == 1 {
                         match map.get(row, col) {
-                            Some((Cell::SplitH,_,_)) => string += "-",
-                            Some((Cell::SplitV,_,_)) => string += "|",
-                            Some((Cell::MirrorB,_,_)) => string += "\\",
-                            Some((Cell::MirrorF,_,_)) => string += "/",
-                            _ => string+= ".",
+                            Some((Cell::SplitH, _, _)) => string += "-",
+                            Some((Cell::SplitV, _, _)) => string += "|",
+                            Some((Cell::MirrorB, _, _)) => string += "\\",
+                            Some((Cell::MirrorF, _, _)) => string += "/",
+                            _ => string += ".",
                         }
-                    } else if let Some((_, from, _)) = map.get(row,col) && 
-                        *from != 0
+                    } else if let Some((_, from, _)) = map.get(row, col)
+                        && *from != 0
                     {
-                        string += 
-                        match (i,j) {
-                            (0,0) => "┌",
-                            (0,2) => "┐",
-                            (2,0) => "└",
-                            (2,2) => "┘",
-                            _ => " "
+                        string += match (i, j) {
+                            (0, 0) => "┌",
+                            (0, 2) => "┐",
+                            (2, 0) => "└",
+                            (2, 2) => "┘",
+                            _ => " ",
                         };
                     } else {
                         string += " ";
                     }
-
                 }
             }
             string += "\n";
@@ -140,21 +127,27 @@ pub fn _display(map: &Grid<(Cell, u8, u8)>) -> String {
     string
 }
 
-pub fn add(rays: &mut Vec<(i32,i32,Dir)>, history: &mut Vec<(i32,i32,Dir)>, item: (i32,i32,Dir)) {
+pub fn add(
+    rays: &mut Vec<(i32, i32, Dir)>,
+    history: &mut Vec<(i32, i32, Dir)>,
+    item: (i32, i32, Dir),
+) {
     if !rays.contains(&item) && !history.contains(&item) {
         rays.push(item);
         history.push(item);
     }
 }
 
-pub fn process(map: &mut Grid<(Cell, u8, u8)>) {
+pub fn process(map: &mut Grid<(Cell, u8, u8)>, starts: Vec<(i32, i32, Dir)>) {
     let mut rays = Vec::new();
     let mut history = Vec::new();
     //    1
     // 2     8
     //    4
-    add(&mut rays, &mut history, (0, 0, Dir::Left));
-    while let Some((row,col,from)) =  rays.pop() {
+    for start in starts {
+        add(&mut rays, &mut history, start);
+    }
+    while let Some((row, col, from)) = rays.pop() {
         if let Some((cell, cfrom, cto)) = map.get_mut(row, col) {
             // if cell.1 & from as u8 != 0 {
             //     continue;
@@ -165,74 +158,72 @@ pub fn process(map: &mut Grid<(Cell, u8, u8)>) {
                 //  '.'
                 match from {
                     Dir::Up => {
-                        add(&mut rays,  &mut history,(row+1, col, Dir::Up));
+                        add(&mut rays, &mut history, (row + 1, col, Dir::Up));
                         to |= Dir::Down as u8;
-                    },
+                    }
                     Dir::Left => {
-                        add(&mut rays,  &mut history,(row, col+1, Dir::Left));
+                        add(&mut rays, &mut history, (row, col + 1, Dir::Left));
                         to |= Dir::Right as u8;
-                    },
+                    }
                     Dir::Down => {
-                        add(&mut rays,  &mut history,(row-1, col, Dir::Down));
+                        add(&mut rays, &mut history, (row - 1, col, Dir::Down));
                         to |= Dir::Up as u8;
-                        
-                    },
+                    }
                     Dir::Right => {
-                        add(&mut rays,  &mut history,(row, col-1, Dir::Right));
+                        add(&mut rays, &mut history, (row, col - 1, Dir::Right));
                         to |= Dir::Left as u8;
-                    },
+                    }
                 }
             } else if *cell == Cell::MirrorB {
                 //  '\'
                 match from {
                     Dir::Up => {
-                        add(&mut rays,  &mut history,(row, col+1, Dir::Left));
+                        add(&mut rays, &mut history, (row, col + 1, Dir::Left));
                         to |= Dir::Right as u8;
-                    },
+                    }
                     Dir::Left => {
-                        add(&mut rays,  &mut history,(row+1, col, Dir::Up));
+                        add(&mut rays, &mut history, (row + 1, col, Dir::Up));
                         to |= Dir::Down as u8;
-                    },
+                    }
                     Dir::Down => {
-                        add(&mut rays,  &mut history,(row, col-1, Dir::Right));
+                        add(&mut rays, &mut history, (row, col - 1, Dir::Right));
                         to |= Dir::Left as u8;
-                        
-                    },
+                    }
                     Dir::Right => {
-                        add(&mut rays,  &mut history,(row-1, col, Dir::Down));
+                        add(&mut rays, &mut history, (row - 1, col, Dir::Down));
                         to |= Dir::Up as u8;
-                    },
+                    }
                 }
             } else if *cell == Cell::MirrorF {
                 //  '/'
                 match from {
                     Dir::Up => {
-                        add(&mut rays,  &mut history,(row, col-1, Dir::Right));
+                        add(&mut rays, &mut history, (row, col - 1, Dir::Right));
                         to |= Dir::Left as u8;
-                    },
+                    }
                     Dir::Left => {
-                        add(&mut rays,  &mut history,(row-1, col, Dir::Down));
+                        add(&mut rays, &mut history, (row - 1, col, Dir::Down));
                         to |= Dir::Up as u8;
-                    },
+                    }
                     Dir::Down => {
-                        add(&mut rays,  &mut history,(row, col+1, Dir::Left));
+                        add(&mut rays, &mut history, (row, col + 1, Dir::Left));
                         to |= Dir::Right as u8;
-                    },
+                    }
                     Dir::Right => {
-                        add(&mut rays,  &mut history,(row+1, col, Dir::Up));
+                        add(&mut rays, &mut history, (row + 1, col, Dir::Up));
                         to |= Dir::Down as u8;
-                    },
+                    }
                 }
             } else if *cell == Cell::SplitH {
                 //  '-'
-                add(&mut rays,  &mut history,(row,col-1, Dir::Right));
-                add(&mut rays,  &mut history,(row,col+1, Dir::Left));
+                add(&mut rays, &mut history, (row, col - 1, Dir::Right));
+                add(&mut rays, &mut history, (row, col + 1, Dir::Left));
                 to |= Dir::Right as u8;
                 to |= Dir::Left as u8;
             } else if *cell == Cell::SplitV {
                 //  '|'
-                add(&mut rays,  &mut history,(row-1,col, Dir::Down));
-                add(&mut rays,  &mut history,(row+1,col, Dir::Up));
+                add(&mut rays, &mut history, (row - 1, col, Dir::Down));
+                add(&mut rays, &mut history, (row + 1, col, Dir::Up));
                 to |= Dir::Down as u8;
                 to |= Dir::Up as u8;
             }
@@ -253,15 +244,46 @@ pub mod part1 {
 
     pub fn solve(file: String) -> u64 {
         let mut map = parse(file);
-        println!("{}", _display(&map));
-        process(&mut map);
-        println!("{}", _display(&map));
-        map.iter().filter(|(_cell, active,_)| active!=&0).count() as u64
+        // println!("{}", _display(&map));
+        process(&mut map, vec![(0, 0, Dir::Left)]);
+        // println!("{}", _display(&map));
+        map.iter().filter(|(_cell, active, _)| active != &0).count() as u64
     }
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use super::*;
+
+    pub fn solve(file: String) -> u64 {
+        let map = parse(file);
+        // println!("{}", _display(&map));
+        let mut starts = Vec::new();
+        for col in 0..map.cols() {
+            starts.push((0, col, Dir::Up));
+            starts.push((map.rows() - 1, col, Dir::Down));
+        }
+        for row in 0..map.rows() {
+            starts.push((row, 0, Dir::Left));
+            starts.push((row, map.cols() - 1, Dir::Right));
+        }
+
+        let rank: Vec<u64> = starts
+            .iter()
+            .inspect(|s| print!("{s:?}"))
+            .map(|start| {
+                let mut temp_map = map.clone();
+                process(
+                    &mut temp_map,
+                    vec![(start.0 as i32, start.1 as i32, start.2)],
+                );
+                temp_map
+                    .iter()
+                    .filter(|(_cell, active, _)| active != &0)
+                    .count() as u64
+            })
+            .inspect(|s| println!(" => {s:?}"))
+            .collect();
+
+        *rank.iter().max().unwrap()
     }
 }
--- 2023/2023-16/tests/tests.rs
+++ 2023/2023-16/tests/tests.rs
@@ -13,5 +13,5 @@ test_case_n!(example,
 .|....-|.\\
 ..//.|....",
     part1: (part1::solve, 46),
-    part2: (part2::solve, 0)
+    part2: (part2::solve, 51)
 );
