70ca6be1a0707596873d6d7d4b5f9d4a9ce14efe
Sittisak Sinprem <ssinprem@celestica.com>
2026-08-17 18:14:21 +0700

    2023-21: implment part2 correct but very slow

--- 2023/2023-21/src/lib.rs
+++ 2023/2023-21/src/lib.rs
@@ -1,21 +1,27 @@
 use grid::*;
+use rayon::prelude::*;
 
-pub fn parse(file: String) -> (Grid<bool>, (usize, usize)) {
+pub fn parse(file: String) -> (Grid<bool>, (isize, isize)) {
     let mut map = Grid::new(0, 0);
-    let mut start = (0, 0);
+    let mut start = (0_isize, 0_isize);
     file.lines()
         .filter(|line| !line.is_empty())
         .enumerate()
         .for_each(|(row, line)| {
             if line.contains("S") {
-                start = (row, line.chars().position(|c| c == 'S').unwrap())
+                start = (
+                    row as isize,
+                    line.chars().position(|c| c == 'S').unwrap() as isize
+                )
             }
             map.push_row(line.chars().map(|c| c != '#').collect());
         });
     (map, start)
 }
 
-pub fn _display(map: &Grid<bool>, list: &[(usize, usize)]) -> String {
+
+
+pub fn _display(map: &Grid<bool>, list: &[(isize, isize)]) -> String {
     let mut string = "".to_string();
 
     let rows = map.rows();
@@ -23,7 +29,7 @@ pub fn _display(map: &Grid<bool>, list: &[(usize, usize)]) -> String {
 
     for row in 0..rows {
         for col in 0..cols {
-            if list.contains(&(row, col)) {
+            if list.contains(&(row as isize, col as isize)) {
                 string += "O";
             } else if let Some(c) = map.get(row, col)
                 && *c
@@ -42,17 +48,16 @@ pub fn _display(map: &Grid<bool>, list: &[(usize, usize)]) -> String {
 pub mod part1 {
     use super::*;
 
-    fn process(map: &Grid<bool>, list: &[(usize, usize)]) -> Vec<(usize, usize)> {
+    pub fn process(map: &Grid<bool>, list: &[(isize, isize)]) -> Vec<(isize, isize)> {
         let mut new_list = Vec::new();
 
         for node in list {
-            let node = (node.0 as isize, node.1 as isize);
             for d in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
                 let new_node = (node.0 + d.0, node.1 + d.1);
                 if let Some(is_path) = map.get(new_node.0, new_node.1)
                     && *is_path
                 {
-                    let new_node = (new_node.0 as usize, new_node.1 as usize);
+                    let new_node = (new_node.0 as isize, new_node.1 as isize);
                     if !new_list.contains(&new_node) {
                         new_list.push(new_node);
                     }
@@ -63,12 +68,12 @@ pub mod part1 {
         new_list
     }
 
-    pub fn solve(file: String) -> u64 {
+    pub fn solve(file: String, cnt: u64) -> u64 {
         let (map, start) = parse(file);
         let mut list = vec![start];
         println!("{}", _display(&map, &list));
 
-        for _i in 0..64 {
+        for _i in 0..cnt {
             list = process(&map, &list);
         }
         println!("{}", _display(&map, &list));
@@ -78,7 +83,54 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use super::*;
+    use std::collections::HashSet;
+
+    pub fn process(map: &Grid<bool>, list: &[(isize, isize)]) -> Vec<(isize, isize)> {
+        let rows = map.rows() as isize;
+        let cols = map.cols() as isize;
+        list
+            .par_iter()
+            .flat_map(|node| {
+                let mut nodes = Vec::new();
+                for d in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+                    let new_node = (node.0 + d.0, node.1 + d.1);
+                    let mut map_r = new_node.0;
+                    let mut map_c = new_node.1;
+                    while map_r < 0 {
+                        map_r += rows
+                    }
+                    while map_r >= rows {
+                        map_r -= rows
+                    }
+                    while map_c < 0 {
+                        map_c += cols
+                    }
+                    while map_c >= cols {
+                        map_c -= cols
+                    }
+                    if let Some(is_path) = map.get(map_r, map_c)
+                        && *is_path
+                    {
+                        nodes.push((new_node.0 as isize, new_node.1 as isize));
+                    }
+                }
+                nodes
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .collect::<HashSet<_>>()
+            .into_iter()
+            .collect::<Vec<(isize,isize)>>()
+    }
+
+    pub fn solve(file: String, cnt: u64) -> u64 {
+        let (map, start) = parse(file);
+        let mut list = vec![start];
+        for _i in 0..cnt {
+            list = process(&map, &list);
+            println!("{_i} {}", list.len());
+        }
+        list.len() as u64
     }
 }
--- 2023/2023-21/tests/tests.rs
+++ 2023/2023-21/tests/tests.rs
@@ -13,6 +13,13 @@ test_case_n!(example,
 .##.#.####.
 .##..##.##.
 ...........",
-    part1: (part1::solve, 42),
-    part2: (part2::solve, 0)
+    part1_6: (part1::solve, 16, 6),
+    part1_64: (part1::solve, 42, 64),
+    part2_6: (part2::solve, 16, 6),
+    part2_10: (part2::solve, 50, 10),
+    part2_50: (part2::solve, 1594, 50),
+    part2_100: (part2::solve, 6536, 100),
+    part2_500: (part2::solve, 167004, 500),
+    part2_1000: (part2::solve, 668697, 1000),
+    part2_5000: (part2::solve, 16733044, 5000)
 );
