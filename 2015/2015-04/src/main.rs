6ee308b6791a8ae602015bb6613168c6cb914402
Sittisak Sinprem <ssinprem@celestica.com>
2026-08-28 15:03:07 +0700

    2015-04: cleared part2

--- 2015/2015-04/src/main.rs
+++ 2015/2015-04/src/main.rs
@@ -1,8 +1,8 @@
-use the_ideal_stocking_stuffer::*;
 use std::error::Error;
 use std::fs::read_to_string;
+use the_ideal_stocking_stuffer::*;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let file = read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/input.txt"))?;
     println!("part1 = {}", part1::solve(file.clone()));
     println!("part2 = {}", part2::solve(file.clone()));
--- 2015/2015-04/tests/tests.rs
+++ 2015/2015-04/tests/tests.rs
@@ -4,11 +4,11 @@ use utility::test_case_n;
 test_case_n!(example1,
 "abcdef",
     part1: (part1::solve, 609043),
-    part2: (part2::solve, 0)
+    part2: (part2::solve, 6742839)
 );
 
 test_case_n!(example2,
 "pqrstuv",
     part1: (part1::solve, 1048970),
-    part2: (part2::solve, 0)
+    part2: (part2::solve, 5714438)
 );
