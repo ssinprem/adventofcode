8b78ddc8d43a920e3d96413c328578b1945133f8
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-04 13:59:27 +0700

    2015-12: use serde_json, cleared part2

--- 2015/2015-12/src/lib.rs
+++ 2015/2015-12/src/lib.rs
@@ -15,7 +15,36 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use serde_json::Value;
+
+    pub fn extract_sum(objects : Value) -> i64 {
+        if objects.is_array() {
+            objects.as_array().map(|item| {
+                item.iter().map(|value| {
+                    extract_sum(value.clone())
+                }).sum()
+            }).iter().sum()
+        } else if objects.is_object() {
+            if objects.as_object().into_iter().find(| obj | {
+                obj.values().any(|value| value == &"red")
+            }).is_some() {
+                0
+            } else {
+                objects.as_object().into_iter().map(|obj| {
+                    obj.values().map(|value| {
+                        extract_sum(value.clone())
+                    }).sum::<i64>()
+                }).sum()
+            }
+        } else if objects.is_i64() {
+            objects.as_i64().unwrap()
+        } else {
+            0
+        }
+    }
+
+    pub fn solve(file: String) -> i64 {
+        let objects: Value = serde_json::from_str(file.as_str()).expect("JSON cannot parse");
+        extract_sum(objects)
     }
 }
--- 2015/2015-12/tests/tests.rs
+++ 2015/2015-12/tests/tests.rs
@@ -1,13 +1,22 @@
 use jsabacusframwork_io::*;
 
 #[test]
-fn example() {
-    assert_eq!(6, part1::solve("[1,2,3]".to_string()));
-    assert_eq!(6, part1::solve("{\"a\":2,\"b\":4} ".to_string()));
-    assert_eq!(3, part1::solve("[[[3]]]".to_string()));
-    assert_eq!(3, part1::solve("{\"a\":{\"b\":4},\"c\":-1}".to_string()));
-    assert_eq!(0, part1::solve("{\"a\":[-1,1]}".to_string()));
-    assert_eq!(0, part1::solve("[-1,{\"a\":1}]".to_string()));
-    assert_eq!(0, part1::solve("[]".to_string()));
-    assert_eq!(0, part1::solve("{}".to_string()));
+fn example_part1() {
+    assert_eq!(6, part1::solve(r#"[1,2,3]"#.to_string()));
+    assert_eq!(6, part1::solve(r#"{"a":2,"b":4} "#.to_string()));
+    assert_eq!(3, part1::solve(r#"[[[3]]]"#.to_string()));
+    assert_eq!(3, part1::solve(r#"{"a":{"b":4},"c":-1}"#.to_string()));
+    assert_eq!(0, part1::solve(r#"{"a":[-1,1]}"#.to_string()));
+    assert_eq!(0, part1::solve(r#"[-1,{"a":1}]"#.to_string()));
+    assert_eq!(0, part1::solve(r#"[]"#.to_string()));
+    assert_eq!(0, part1::solve(r#"{}"#.to_string()));
+}
+
+#[test]
+fn example_part2() {
+    assert_eq!(6, part2::solve("[1,2,3]".to_string()));
+    assert_eq!(4, part2::solve(r#"[1,{"c":"red","b":2},3]"#.to_string()));
+    assert_eq!(0, part2::solve(r#"{"d":"red","e":[1,2,3,4],"f":5}"#.to_string()));
+    assert_eq!(6, part2::solve(r#"[1,"red",5]"#.to_string()));
+    
 }
