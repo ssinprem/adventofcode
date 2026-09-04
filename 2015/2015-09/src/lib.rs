9e03285513f41569d9fca1f8aff96ee31a72d14d
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-04 09:29:40 +0700

    2015-09: cleared part1

---
+++ 2015/2015-09/src/lib.rs
@@ -0,0 +1,74 @@
+use std::collections::HashMap;
+use petgraph::graph::UnGraph;
+use petgraph::prelude::NodeIndex;
+use regex::Regex;
+
+pub fn parse(file: String) -> (UnGraph<String, u32>, HashMap<String, NodeIndex>) {
+    let mut graph : UnGraph<String, u32> = UnGraph::<String, u32>::new_undirected();
+    let mut id: HashMap<String, NodeIndex> = HashMap::new();
+    let regex = Regex::new(r"(\w+) to (\w+) = (\d+)").unwrap();
+
+    file.lines().for_each(|line| {
+        if let Some(cap) = regex.captures(line)
+        && let Some(w1) = cap.get(1)
+        && let Some(w2) = cap.get(2)
+        && let Some(v) = cap.get(3)
+        {
+            let id1 = if let Some(id) = id.get(w1.as_str()) {
+                *id
+            } else {
+                let id1 = graph.add_node(w1.as_str().to_string());
+                id.insert(w1.as_str().to_string(), id1);
+                id1
+            };
+            let id2 = if let Some(id) = id.get(w2.as_str()) {
+                *id
+            } else {
+                let id2 = graph.add_node(w2.as_str().to_string());
+                id.insert(w2.as_str().to_string(), id2);
+                id2
+            };
+            graph.add_edge(id1, id2, v.as_str().parse::<u32>().unwrap());
+        }
+    });
+
+    (graph, id)
+}
+
+pub mod part1 {
+    use petgraph::visit::EdgeRef;
+    use super::*;
+    pub fn solve(file: String) -> u64 {
+        let (graph, id) = parse(file);
+        let mut working_paths: Vec<(Vec<NodeIndex>, u32)> = id.iter().map(|node| (vec![*node.1], 0)).collect();
+        let node_count = graph.node_count();
+        let mut scores = Vec::<u32>::new();
+        while let Some((path, score)) = working_paths.pop() {
+            if path.len() == node_count {
+                scores.push(score);
+                continue;
+            }
+            let last = path.last().unwrap();
+            let edges = graph.neighbors_undirected(*last);
+
+            for edge in edges {
+                if path.contains(&edge) {
+                    continue;
+                }
+                let mut new_path = path.clone();
+                new_path.push(edge);
+                let edge_score = graph.edges(*last).find(|e| e.source() == edge || e.target() == edge).unwrap();
+                let edge_score = edge_score.weight();
+                let new_score = score + *edge_score;
+                working_paths.push((new_path, new_score));
+            }
+        }
+        scores.into_iter().min().unwrap() as u64
+    }
+}
+
+pub mod part2 {
+    pub fn solve(_file: String) -> u64 {
+        0
+    }
+}
---
+++ 2015/2015-09/tests/tests.rs
@@ -0,0 +1,10 @@
+use all_in_a_single_night::*;
+use utility::test_case_n;
+
+test_case_n!(example,
+"London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141",
+    part1: (part1::solve, 605),
+    part2: (part2::solve, 0)
+);
