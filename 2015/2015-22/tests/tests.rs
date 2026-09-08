d70f216cb196b62dfe52860b5d27342ee7e24297
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-08 16:30:14 +0700

    2015-22: add test case for part2

--- 2015/2015-22/tests/tests.rs
+++ 2015/2015-22/tests/tests.rs
@@ -1,15 +1,23 @@
 use wizard_simulator_20xx::*;
 use utility::test_case_n;
 
-test_case_n!(example,
+test_case_n!(example1,
 "Hit Points: 13
 Damage: 8",
-    part1: (part1::solve, Some(226), 10, 250),
-    part2: (part2::solve, None, 10, 250)
+    part1: (part1::solve, Some(226), 10, 250)
 );
 test_case_n!(example2,
 "Hit Points: 14
 Damage: 8",
-    part1: (part1::solve, Some(641), 10, 250),
-    part2: (part2::solve, None, 10, 250)
+    part1: (part1::solve, Some(641), 10, 250)
+);
+test_case_n!(example1,
+"Hit Points: 10
+Damage: 5",
+    part2: (part2::solve, Some(226), 10, 250)
+);
+test_case_n!(example2,
+"Hit Points: 14
+Damage: 8",
+    part2: (part2::solve, Some(339), 10, 500)
 );
