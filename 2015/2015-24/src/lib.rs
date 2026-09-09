31bde9ba045df020e7c71ff2e33f3d1c6ae57b04
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-09 10:35:08 +0700

    2015-24: use combinations in itertools to speed up.

--- 2015/2015-24/src/lib.rs
+++ 2015/2015-24/src/lib.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 
 pub fn parse(file: String) -> Vec<u64> {
     file.lines().filter_map(|line| {
@@ -9,32 +10,29 @@ pub fn parse(file: String) -> Vec<u64> {
     }).collect()
 }
 
+fn find_best_qe(packages: &[u64], num_groups: u64) -> u64 {
+    let total_weight: u64 = packages.iter().sum();
+    let target_weight = total_weight / num_groups;
+
+    for k in 1..packages.len() {
+        if let Some(qe) = packages
+            .iter()
+            .combinations(k)
+            .filter(|c| c.iter().copied().sum::<u64>() == target_weight)
+            .map(|c| c.iter().copied().product::<u64>())
+            .min() {
+            return qe;
+        }
+    }
+    unreachable!()
+}
+
 pub mod part1 {
     use super::*;
 
     pub fn solve(file: String) -> u64 {
         let n_list = parse(file);
-        let weight = n_list.clone().into_iter().sum::<u64>() / 3;
-
-        let mut possible = vec![vec![]];
-        for n in n_list.clone() {
-            let mut new_possible = vec![];
-
-            while let Some(mut pos) = possible.pop() {
-                new_possible.push(pos.clone());
-                pos.push(n);
-                if pos.clone().into_iter().sum::<u64>() <= weight {
-                    new_possible.push(pos);
-                }
-            }
-            possible = new_possible;
-        }
-
-        possible = possible.into_iter().filter(|pos| pos.into_iter().copied().sum::<u64>() == weight).collect();
-        // let target = possible.iter().min_by_key(|v| v.into_iter().copied().product::<u64>()).unwrap();
-        let target = possible.iter().min_by_key(|v| v.len()).unwrap();
-        println!("{target:?}");
-        target.into_iter().copied().product()
+        find_best_qe(&n_list, 3)
     }
 }
 
@@ -43,30 +41,6 @@ pub mod part2 {
 
     pub fn solve(file: String) -> u64 {
         let n_list = parse(file);
-        let weight = n_list.clone().into_iter().sum::<u64>() / 4;
-
-        let mut possible = vec![vec![]];
-        for n in n_list.clone() {
-            let mut new_possible = vec![];
-
-            while let Some(mut pos) = possible.pop() {
-                new_possible.push(pos.clone());
-                pos.push(n);
-                if pos.clone().into_iter().sum::<u64>() <= weight {
-                    new_possible.push(pos);
-                }
-            }
-            possible = new_possible;
-        }
-
-        possible = possible.into_iter().filter(|pos| pos.into_iter().copied().sum::<u64>() == weight).collect();
-        // let target = possible.iter().min_by_key(|v| v.into_iter().copied().product::<u64>()).unwrap();
-        let target = possible.iter()
-            .min_by_key(|v|
-                (v.len()*1000) as u64 +
-                v.into_iter().copied().product::<u64>()
-            ).unwrap();
-        println!("{target:?}");
-        target.into_iter().copied().product()
+        find_best_qe(&n_list, 4)
     }
 }
