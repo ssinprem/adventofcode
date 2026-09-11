d7b6d1fb2a0b43a775ef5ee7d793f12cd350db68
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-11 17:47:11 +0700

    2016-01: cleared part2

--- 2016/2016-01/src/lib.rs
+++ 2016/2016-01/src/lib.rs
@@ -42,7 +42,50 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
+    pub fn solve(file: String) -> u64 {
+        let mut head = (0, 1); // N E S W
+        let mut pos: (i32, i32) = (0, 0);
+        let mut history = vec![(0,0)];
+        
+        let steps = file
+            .clone()
+            .split(",")
+            .map(|step| {
+                let (h, n) = step.trim().split_at(1);
+                let n = n.parse::<u32>().unwrap();
+                (h.chars().next().unwrap(), n)
+            })
+            .collect::<Vec<(char, u32)>>();
+
+        for (h, n) in steps {
+            if h == 'L' {
+                head = match head {
+                    (0,1) /* N */=> (-1,0), /* W */
+                    (1,0) /* E */=> (0,1), /* N */
+                    (0,-1) /* S */=> (1,0), /* E */
+                    (-1,0) /* W */=> (0,-1), /* S */
+                    _ => unreachable!()
+                }
+            } else if h == 'R' {
+                head = match head {
+                    (0,1) /* N */=> (1,0), /* E */
+                    (1,0) /* E */=> (0,-1), /* S */
+                    (0,-1) /* S */=> (-1,0), /* W */
+                    (-1,0) /* W */=> (0,1), /* N */
+                    _ => unreachable!()
+                }
+            }
+            for _ in 0..n {
+                pos.0 += head.0;
+                pos.1 += head.1;
+
+                if history.contains(&pos) {
+                    return (pos.0.abs() + pos.1.abs()) as u64;
+                }
+                history.push(pos.clone());
+            }
+        }
+
         0
     }
 }
--- 2016/2016-01/tests/tests.rs
+++ 2016/2016-01/tests/tests.rs
@@ -4,3 +4,4 @@ use utility::test_case_n;
 test_case_n!(example1, "R2, L3", part1: (part1::solve, 5));
 test_case_n!(example2, "R2, R2, R2", part1: (part1::solve, 2));
 test_case_n!(example3, "R5, L5, R5, R3", part1: (part1::solve, 12));
+test_case_n!(example1, "R8, R4, R4, R8", part2: (part2::solve, 4));
