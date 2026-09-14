27ab79b53b3557b9ef9fb595c8a3a28907e5142b
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-14 16:39:34 +0700

    2016-06: refactor to move duplicate code to function

--- 2016/2016-06/src/lib.rs
+++ 2016/2016-06/src/lib.rs
@@ -1,22 +1,26 @@
+use std::collections::HashMap;
+
+pub fn calculate_distibution(file: String) -> Vec<HashMap<char, usize>> {
+    let n = file.lines().next().unwrap().len();
+    let mut history = vec![HashMap::<char, usize>::new(); n];
+    file.lines()
+        .filter(|line| !line.is_empty())
+        .for_each(|line| {
+            line.chars().enumerate().for_each(|(i, char)| {
+                history[i]
+                    .entry(char)
+                    .and_modify(|num| *num += 1)
+                    .or_insert(1);
+            });
+        });
+    history
+}
+
 pub mod part1 {
-    use std::collections::HashMap;
+    use super::*;
 
     pub fn solve(file: String) -> String {
-        let n = file.lines().next().unwrap().len();
-        let mut history = vec![HashMap::<char, usize>::new(); n];
-
-        file.lines()
-            .filter(|line| !line.is_empty())
-            .for_each(|line| {
-                line.chars().enumerate().for_each(|(i, char)| {
-                    history[i]
-                        .entry(char)
-                        .and_modify(|num| *num += 1)
-                        .or_insert(1);
-                });
-            });
-
-        history
+        calculate_distibution(file)
             .iter()
             .map(|hm| {
                 let (char, _) = hm.iter().max_by_key(|(_, num)| *num).unwrap();
@@ -27,24 +31,10 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    use std::collections::HashMap;
+    use super::*;
 
     pub fn solve(file: String) -> String {
-        let n = file.lines().next().unwrap().len();
-        let mut history = vec![HashMap::<char, usize>::new(); n];
-
-        file.lines()
-            .filter(|line| !line.is_empty())
-            .for_each(|line| {
-                line.chars().enumerate().for_each(|(i, char)| {
-                    history[i]
-                        .entry(char)
-                        .and_modify(|num| *num += 1)
-                        .or_insert(1);
-                });
-            });
-
-        history
+        calculate_distibution(file)
             .iter()
             .map(|hm| {
                 let (char, _) = hm.iter().min_by_key(|(_, num)| *num).unwrap();
