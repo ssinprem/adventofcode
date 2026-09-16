f01304e85ab05dc4ec846efc44757f707676ad23
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-16 10:33:10 +0700

    2016-09: part2 pass, but very slow.

--- 2016/2016-09/src/lib.rs
+++ 2016/2016-09/src/lib.rs
@@ -9,7 +9,6 @@ pub mod part1 {
         let regex = Regex::new(r"(?:^\(([0-9]+)x([0-9]+)\))|(^[A-Z]+)").unwrap();
         while !input.is_empty() {
             if let Some(caps) = regex.captures(&input) {
-                println!("{:?}", caps);
                 if let Some(size_txt) = caps.get(1)
                     && let Some(times_txt) = caps.get(2)
                     && let Ok(size) = size_txt.as_str().parse::<u32>()
@@ -38,7 +37,36 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    pub fn solve(_file: String) -> u64 {
-        0
+    use regex::Regex;
+
+    pub fn decompress(file: String) -> String {
+        let mut string = file.to_string();
+
+        let regex = Regex::new(r"\(([0-9]+)x([0-9]+)\)(.*)").unwrap();
+        while let Some(caps)  = regex.captures(&string) {
+            println!("{} {}", string.len(), string.chars().into_iter().filter(|c| *c=='(').count());
+            if let Some(size_txt) = caps.get(1)
+                && let Some(times_txt) = caps.get(2)
+                && let Ok(size) = size_txt.as_str().parse::<usize>()
+                && let Ok(times) = times_txt.as_str().parse::<usize>()
+                && let Some(text) = caps.get(3)
+                && text.len() >= size
+            {
+                let (front, _) = string.split_once("(").expect("cannot find '('");
+                let (_, output) = string.split_once(")").expect("cannot find ')'");
+                let mut output = output.to_string();
+                let back = output.split_off(size);
+                output = output.repeat(times);
+                string = front.to_string() + output.as_str() + back.as_str();
+            } else {
+                unreachable!()
+            }
+        }
+        // println!("{string}");
+        string
+    }
+
+    pub fn solve(file: String) -> u64 {
+        decompress(file).len() as u64
     }
 }
--- 2016/2016-09/tests/tests.rs
+++ 2016/2016-09/tests/tests.rs
@@ -7,3 +7,9 @@ test_case_n!(example3, "(3x3)XYZ", part1: (part1::solve, 9));
 test_case_n!(example4, "A(2x2)BCD(2x2)EFG", part1: (part1::solve, 11));
 test_case_n!(example5, "(6x1)(1x3)A", part1: (part1::solve, 6));
 test_case_n!(example6, "X(8x2)(3x3)ABCY", part1: (part1::solve, 18));
+
+
+test_case_n!(example1, "(3x3)XYZ", part2: (part2::solve, 9));
+test_case_n!(example2, "X(8x2)(3x3)ABCY", part2: (part2::solve, 20));
+test_case_n!(example3, "(27x12)(20x12)(13x14)(7x10)(1x12)A", part2: (part2::solve, 241920));
+test_case_n!(example4, "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", part2: (part2::solve, 445));
