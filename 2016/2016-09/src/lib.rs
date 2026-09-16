a59452e1a4b0bee5eb431d9432a809a4bcb1131a
Sittisak Sinprem <ssinprem@celestica.com>
2026-09-16 10:48:27 +0700

    2016-09: cleared part2, focus only len, no need get actual content

--- 2016/2016-09/src/lib.rs
+++ 2016/2016-09/src/lib.rs
@@ -39,34 +39,34 @@ pub mod part1 {
 pub mod part2 {
     use regex::Regex;
 
-    pub fn decompress(file: String) -> String {
-        let mut string = file.to_string();
-
+    pub fn decompress_len(file: String) -> u64 {
+        let string = file.to_string();
         let regex = Regex::new(r"\(([0-9]+)x([0-9]+)\)(.*)").unwrap();
-        while let Some(caps)  = regex.captures(&string) {
-            println!("{} {}", string.len(), string.chars().into_iter().filter(|c| *c=='(').count());
+        if let Some(caps)  = regex.captures(&string) {
             if let Some(size_txt) = caps.get(1)
                 && let Some(times_txt) = caps.get(2)
                 && let Ok(size) = size_txt.as_str().parse::<usize>()
-                && let Ok(times) = times_txt.as_str().parse::<usize>()
+                && let Ok(times) = times_txt.as_str().parse::<u64>()
                 && let Some(text) = caps.get(3)
                 && text.len() >= size
             {
                 let (front, _) = string.split_once("(").expect("cannot find '('");
-                let (_, output) = string.split_once(")").expect("cannot find ')'");
-                let mut output = output.to_string();
-                let back = output.split_off(size);
-                output = output.repeat(times);
-                string = front.to_string() + output.as_str() + back.as_str();
+                let (_, target) = string.split_once(")").expect("cannot find ')'");
+                let mut target = target.to_string();
+                let back = target.split_off(size);
+                return front.len() as u64
+                 + decompress_len(target) * times
+                 + decompress_len(back)
+                ;
             } else {
                 unreachable!()
             }
+        } else {
+            file.len() as u64
         }
-        // println!("{string}");
-        string
     }
 
     pub fn solve(file: String) -> u64 {
-        decompress(file).len() as u64
+        decompress_len(file)
     }
 }
